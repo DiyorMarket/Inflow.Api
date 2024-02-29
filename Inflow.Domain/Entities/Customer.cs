12a76d745649d2004dd7380e9040498a1ceb0c59
Miraziz <khidoyatov1997@gmail.com>
2024-02-29 15:57:57 +0500

    update entities

--- Inflow.Domain/Entities/Customer.cs
+++ Inflow.Domain/Entities/Customer.cs
@@ -2,10 +2,11 @@
 {
     public class Customer : EntityBase
     {
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
         public string? LastName { get; set; }
-        public string? PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Company { get; set; }
 
-        public virtual ICollection<Sale>? Sales { get; set; }
+        public virtual ICollection<Sale> Sales { get; set; }
     }
 }
--- Inflow.Domain/Entities/Sale.cs
+++ Inflow.Domain/Entities/Sale.cs
@@ -3,10 +3,11 @@
     public class Sale : EntityBase
     {
         public DateTime SaleDate { get; set; }
+        public decimal TotalPrice { get; set; }
 
         public int CustomerId { get; set; }
-        public Customer? Customer { get; set; }
+        public Customer Customer { get; set; }
 
-        public virtual ICollection<SaleItem>? SaleItems { get; set; }
+        public virtual ICollection<SaleItem> SaleItems { get; set; }
     }
 }
