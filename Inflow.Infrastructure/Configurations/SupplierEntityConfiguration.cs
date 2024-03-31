1c25d4719d10209bcee0341356fb7a2ad2c9ebe0
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-31 14:12:58 +0500

    Added SupplierEntityConfiguration

---
+++ Inflow.Infrastructure/Configurations/SupplierEntityConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Inflow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Inflow.Infrastructure.Configurations
+{
+    internal class SupplierEntityConfiguration : IEntityTypeConfiguration<Supplier>
+    {
+        public void Configure(EntityTypeBuilder<Supplier> builder)
+        {
+            builder.ToTable(nameof(Supplier));
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.FirstName)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(s => s.LastName)
+                .HasMaxLength(255);
+
+            builder.Property(s => s.PhoneNumber)
+                .HasMaxLength(255);
+
+            builder.Property(s => s.Company)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.HasMany(s => s.Supplies)
+                .WithOne(sp => sp.Supplier)
+                .HasForeignKey(sp => sp.SupplierId);
+        }
+    }
+}
