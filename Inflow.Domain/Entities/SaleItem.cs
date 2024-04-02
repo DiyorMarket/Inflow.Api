264b0745d86a828d98787f3d15a9c094bef47428
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-04-02 12:09:23 +0500

    Updated SaleItemMappings

--- Inflow.Domain/Entities/SaleItem.cs
+++ Inflow.Domain/Entities/SaleItem.cs
@@ -6,7 +6,7 @@
         public decimal UnitPrice { get; set; }
 
         public int ProductId { get; set; }
-        public Product? Product { get; set; }
+        public Product Product { get; set; }
 
         public int SaleId { get; set; }
         public Sale? Sale { get; set; }
--- Inflow.Domain/Mappings/SaleItemMappings.cs
+++ Inflow.Domain/Mappings/SaleItemMappings.cs
@@ -7,10 +7,12 @@ namespace Inflow.Domain.Mappings
 {
     public class SaleItemMappings : Profile
     {
-        public SaleItemMappings() 
+        public SaleItemMappings()
         {
             CreateMap<SaleItemDto, SaleItem>();
-            CreateMap<SaleItem, SaleItemDto>();
+            CreateMap<SaleItem, SaleItemDto>()
+                .ForCtorParam(nameof(SaleItemDto.TotalDue), x => x.MapFrom(q => q.Quantity * q.UnitPrice))
+                .ForCtorParam(nameof(SaleItemDto.ProductName), x => x.MapFrom(q => q.Product.Name));
             CreateMap<SaleItemForCreateDto, SaleItem>();
             CreateMap<SaleItemForUpdateDto, SaleItem>();
         }
