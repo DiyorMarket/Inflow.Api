68424eeb2cf899a6881a627980098e1489e84e12
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-04-02 15:17:22 +0500

    Corrected SupplyItem and Updated SupplyItemResourceParameters

--- Inflow.Service/SupplyItemService.cs
+++ Inflow.Service/SupplyItemService.cs
@@ -1,20 +1,20 @@
 ﻿using AutoMapper;
-using DiyorMarket.Domain.DTOs.SupplyItem;
-using DiyorMarket.Domain.Entities;
-using DiyorMarket.Domain.Interfaces.Services;
-using DiyorMarket.Domain.Pagniation;
-using DiyorMarket.Domain.ResourceParameters;
-using DiyorMarket.Domain.Responses;
-using DiyorMarket.Infrastructure.Persistence;
+using Inflow.Domain.DTOs.SupplyItem;
+using Inflow.Domain.Entities;
+using Inflow.Domain.Interfaces.Services;
+using Inflow.Domain.Pagniation;
+using Inflow.Domain.ResourceParameters;
+using Inflow.Domain.Responses;
+using Inflow.Infrastructure;
 
 namespace DiyorMarket.Services
 {
     public class SupplyItemService : ISupplyItemService
     {
         private readonly IMapper _mapper;
-        private readonly DiyorMarketDbContext _context;
+        private readonly InflowDbContext _context;
 
-        public SupplyItemService(IMapper mapper, DiyorMarketDbContext context)
+        public SupplyItemService(IMapper mapper, InflowDbContext context)
         {
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _context = context ?? throw new ArgumentNullException(nameof(context));
@@ -70,7 +70,7 @@ namespace DiyorMarket.Services
 
             var supplyItemDtos = _mapper.Map<List<SupplyItemDto>>(supplyItems);
 
-            var paginatedResult =  new PaginatedList<SupplyItemDto>(supplyItemDtos, supplyItems.TotalCount, supplyItems.CurrentPage, supplyItems.PageSize);
+            var paginatedResult = new PaginatedList<SupplyItemDto>(supplyItemDtos, supplyItems.TotalCount, supplyItems.CurrentPage, supplyItems.PageSize);
 
             return paginatedResult.ToResponse();
         }
